3a2d1fb2d7db8ea514cda608601f0b3a628cfb48
zhaobochen1999@gmail.com <Cooiut11980107>
2020-02-20 10:48:21 -0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # 读文件
-file = "Test/a_example.txt"
+# file = "Test/a_example.txt"
 # file = "Test/b_read_on.txt"
 # file = "Test/c_incunabula.txt"
 # file = "Test/d_tough_choices.txt"
 # file = "Test/e_so_many_books.txt"
-# file = "Test/f_libraries_of_the_world.txt"
+file = "Test/f_libraries_of_the_world.txt"
 
 f = open(file, "r")
 
@@ -17,26 +17,40 @@ book_scores = tuple(int(i) for i in f.readline().rstrip().split(" "))
 # 扫图书馆
 for i in range(libraries):
     for j in f.readline().rstrip().split(" "):
-        library_data[i][0].append(j)
+        library_data[i][0].append(int(j))
     for j in f.readline().rstrip().split(" "):
-        library_data[i][1].append(j)
+        library_data[i][1].append(int(j))
 f.close()
 
-print(library_data)
 # 操作
 
-# min(myList, key=lambda x:abs(x-myNumber))
+already_sign_up = set()
 
+library_score_queue = [i for i in range(libraries)]
 
-# Method 1
-result = []
+library_score_queue = sorted(library_score_queue, key=lambda x: sum([book_scores[i] for i in library_data[x][1]]))
+# 可以除天数
+# print(library_score_queue)
+
+result = [[libraries]] + [[] for i in range(libraries * 2)]
+
+for i in range(len(library_score_queue)):
+    result[(i + 1) * 2 - 1].append(library_score_queue[i])
+    result[(i + 1) * 2 - 1].append(len(library_data[library_score_queue[i]][1]))
+
+    result[(i + 1) * 2] = library_data[library_score_queue[i]][1]
+
+print(result)
 
 # 写文件
 if __name__ == '__main__':
-    output = []
+    # print(library_data)
+
+    output = result
 
     f = open(file.replace("txt", "out"), "w")
-    f.write(str(len(output)) + "\n")
-    f.write(" ".join(output))
+    for i in output:
+        f.write(" ".join([str(j) for j in i]))
+        f.write("\n")
 
     f.close()
